b5e6880561a40c3b2c5d76e4c1810a657dd1420b
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-12-14 23:28:28 +0800

    Misc: using UnrealBuildTool

--- Source/RemWidgetComponentEditor/RemWidgetComponentEditor.Build.cs
+++ Source/RemWidgetComponentEditor/RemWidgetComponentEditor.Build.cs
@@ -1,45 +1,43 @@
-// Copyright RemRemRemRe, All Rights Reserved.
+// Copyright RemRemRemRe. All Rights Reserved.
 
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class RemWidgetComponentEditor : ModuleRules
 {
-	public class RemWidgetComponentEditor : ModuleRules
+	public RemWidgetComponentEditor(ReadOnlyTargetRules target) : base(target)
 	{
-		public RemWidgetComponentEditor(ReadOnlyTargetRules target) : base(target)
-		{
-			ShadowVariableWarningLevel = WarningLevel.Error;
-			IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-			DefaultBuildSettings = BuildSettingsVersion.Latest;
-			CppStandard = CppStandardVersion.EngineDefault;
-			UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-
-			bEnableNonInlinedGenCppWarnings = true;
-			bUseUnity = false;
-
-			PrivateDependencyModuleNames.AddRange(
-				new[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine",
-
-					"Slate",
-					"SlateCore",
-					"UMG",
-					"UMGEditor",
-					"InputCore",
-					"UnrealEd",
-					"PropertyEditor",
-					"EditorStyle",
-					"Kismet",
-
-					"DeveloperSettings",
-					
-					"RemWidgetComponent",
-					"RemCommon",
-					"RemEditorUtilities",
-				}
-			);
-		}
+		ShadowVariableWarningLevel = WarningLevel.Error;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		CppStandard = CppStandardVersion.EngineDefault;
+		UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+		
+		bEnableNonInlinedGenCppWarnings = true;
+		bUseUnity = false;
+		
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				
+				"Slate",
+				"SlateCore",
+				"UMG",
+				"UMGEditor",
+				"InputCore",
+				"UnrealEd",
+				"PropertyEditor",
+				"EditorStyle",
+				"Kismet",
+				
+				"DeveloperSettings",
+				
+				"RemWidgetComponent",
+				"RemCommon",
+				"RemEditorUtilities",
+			}
+		);
 	}
 }
